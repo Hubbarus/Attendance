43412dec597f70c8adf8aea26daaf711ae8d34de
Paul Ponomarev <rocksquare@gmail.com>
2020-08-20 16:30:40 +0300

    GUI

--- src/main/java/com/paul/attendance/MainTest.java
+++ src/main/java/com/paul/attendance/MainTest.java
@@ -1,17 +1,33 @@
 package com.paul.attendance;
 
+import com.paul.attendance.entity.DayEntity;
+import com.paul.attendance.entity.EmployeeEntity;
+import com.paul.attendance.entity.TagsType;
 import com.paul.attendance.service.*;
 
+import java.sql.Date;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MainTest {
 
     public static void main(String[] args) {
-        DayCRUD crud = new DayCRUD();
-        LinkedHashMap<String, Integer> map = crud.getListOfMonthsAndDates();
-        for (Map.Entry entry : map.entrySet()) {
-            System.out.println(entry.getKey() + " " + entry.getValue());
-        }
+//        DayCRUD crud = new DayCRUD();
+//        EmployeeCRUD employeeCRUD = new EmployeeCRUD();
+//        EmployeeEntity abram = employeeCRUD.getById(6);
+//        List<DayEntity> list = crud.getAll();
+//
+//        DayEntity result;
+//        for (DayEntity entity : list) {
+//            if (entity.getEmployeeByEmployee().equals(abram)) {
+//                if (entity.getDate().equals(Date.valueOf("2020-02-07"))) {
+//                    result = entity;
+//                    result.setEvent(TagsType.BUSINESS_TRIP.getCode());
+//                    crud.update(result);
+//                }
+//            }
+//        }
+
     }
 }
